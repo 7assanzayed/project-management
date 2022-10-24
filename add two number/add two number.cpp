0f969979fa6cacaa735acd82bbc4a9cb406405d4
hassan <hsnbdalal222@gmail.com>
2022-10-24 13:27:29 +0200

    added the files

---
+++ add two number/add two number.cpp
@@ -0,0 +1,12 @@
+// add two number.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+void main()
+{
+    int x, y; 
+    cin >> x >> y;
+    cout << x + y;
+}
+
